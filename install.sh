504bd5faaaf1f74465f30f5bcc272889b1de1ffc
Zhebin Jin <Jzhebin@gmail.com>
2017-12-15 19:40:05 +0800

    Add missing .bashrc
    Signed-off-by: Zhebin Jin <Jzhebin@gmail.com>

--- install.sh
+++ install.sh
@@ -13,18 +13,23 @@ fi
 mkdir -p ~/.dotfiles.bak
 mv ~/autoload.d   ~/.dotfiles.bak/ 2>/dev/null
 mv ~/.zshrc ~/.dotfiles.bak/ 2>/dev/null
+mv ~/.bashrc ~/.dotfiles.bak/ 2>/dev/null
 mv ~/.oh-my-zsh ~/.dotfiles.bak/ 2>/dev/null
 mv ~/.vimrc ~/.dotfiles.bak/ 2>/dev/null
 mv ~/.vim ~/.dotfiles.bak/ 2>/dev/null
 mv ~/.gitconfig ~/.dotfiles.bak/ 2>/dev/null
 mv ~/.tmux.conf ~/.dotfiles.bak/ 2>/dev/null
 mv ~/.profile ~/.dotfiles.bak/ 2>/dev/null
+mv zsh/oh-my-zsh ~/.dotfiles.bak/ 2>/dev/null
+mv vim/.vim/bundle ~/.dotfiles.bak/ 2>/dev/null
 
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git zsh/oh-my-zsh/
 cp -r zsh/custom zsh/oh-my-zsh/
 
 git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git vim/.vim/bundle/Vundle.vim/
 
+ln -s $PWD/.profile         ~/.profile
+ln -s $PWD/bash/.bashrc     ~/.bashrc
 ln -s $PWD/autoload.d       ~/autoload.d
 ln -s $PWD/zsh/.zshrc       ~/.zshrc
 ln -s $PWD/zsh/oh-my-zsh    ~/.oh-my-zsh
@@ -32,7 +37,6 @@ ln -s $PWD/vim/.vimrc       ~/.vimrc
 ln -s $PWD/vim/.vim         ~/.vim
 ln -s $PWD/git/.gitconfig   ~/.gitconfig
 ln -s $PWD/tmux/.tmux.conf  ~/.tmux.conf
-ln -s $PWD/.profile    ~/.profile
 
 mkdir -p ~/.local/share/xfce4/terminal/colorschemes
 ln -s $PWD/xfce4/terminal/colorschemes/Moso.theme ~/.local/share/xfce4/terminal/colorschemes/Moso.theme 2>/dev/null
